e7a7d2be65d17176a0f5d7cd747a4e9e67780497
Mahmudxon <mahmudxon98@inbox.ru>
2020-01-27 15:16:23 +0500

    njnjnjnj

--- app/src/main/java/uz/mahmudxon/baselibrary/MainActivity.kt
+++ app/src/main/java/uz/mahmudxon/baselibrary/MainActivity.kt
@@ -2,11 +2,14 @@ package uz.mahmudxon.baselibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.mahmudxon.library.ui.BaseFragment
+import uz.mahmudxon.library.util.startFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
